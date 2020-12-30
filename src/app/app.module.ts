9729b246c69a463bde19d6fa267bf8f9da764043
Sirrele <sirrele@gitfitcode.com>
2020-12-29 21:03:17 -0800

    Another update that actually builds
    The will build for the ios simulator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "@nativescript/angular";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { AppRoutingModule } from '@src/app/app-routing.module';
+import { AppComponent } from '@src/app/app.component';
+import { HomeComponent } from '@src/app/home/home.component';
+
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
