91d796d1027408d211301f51c7b07404267a7160
Sirrele <sirrele@gitfitcode.com>
2021-01-14 01:45:51 -0800

    circle progression bar
    Added a component for the progression of the timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { CircleProgressionBarComponent } from '@src/app/home/circle-progression-bar/circle-progression-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CircleProgressionBarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/circle-progression-bar/circle-progression-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-progression-bar',
+  templateUrl: './circle-progression-bar.component.html',
+  styleUrls: ['./circle-progression-bar.component.scss']
+})
+export class CircleProgressionBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
